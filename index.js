4e396c9b7e82d0a0db84944ce082e3f2612a806f
kostas <eoppep@eoppep.gr>
2023-01-25 16:48:25 +0200

    grid changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const webSocket = new WebSocket('ws://localhost:3001');
 // const textfieldName = document.querySelector('#username')
-// const saveButton = document.querySelector("#saveButton")
-// const textfieldMessage = document.querySelector('#typingArea')
-// const chat = document.querySelector("#chat")
+const saveButton = document.querySelector("#saveButton")
+// // const textfieldMessage = document.querySelector('#typingArea')
+//  const chat = document.querySelector("#chat")
+ const content = document.querySelector('#messagesDisplay')
 // const sendButton = document.querySelector("#paperAirplane")
 // const list = document.querySelector('#list')
 // const refreshButton = document.querySelector('#refreshList')
@@ -10,6 +11,7 @@ const webSocket = new WebSocket('ws://localhost:3001');
 // const uploadButton = document.querySelector('#uploadButton')
 // const storageID = localStorage.getItem('ClientID')
 //const hiddenIDfield = document.querySelector('#userID')
-save.addEventListener('click',e=>{
-    console.log(e)
+saveButton.addEventListener('click',e=>{
+    content.innerHTML+= `            <p id="otherUser">otherUser</p>
+    <p id="mainUser">mainUser</p>`
 })
